00cea266ac5ecc0a720c6149392ba311368db6d9
tymo7256 <tymo7256@pacificu.edu>
2025-10-06 14:59:17 -0700

    Initial commit

---
+++ campus_compass/campus_compassApp.swift
@@ -0,0 +1,17 @@
+//
+//  campus_compassApp.swift
+//  campus_compass
+//
+//  Created by Anna on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct campus_compassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
