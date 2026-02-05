50aa6ee12a9eb6c0993a43b603987c0b4b34f830
NiLyssa Walker <walk1811@pacificu.edu>
2026-02-04 19:55:47 -0800

    added markers to mapview

--- campus_compass/MapView.swift
+++ campus_compass/MapView.swift
@@ -12,10 +12,17 @@ struct MapView: View {
     @StateObject private var locationManager = LocationManager()
     @State private var camera: MapCameraPosition = .automatic
     @State private var hasCenteredOnUser = false   // <- NEW
-
+    
+    let UCLoc = CLLocationCoordinate2D(latitude: 45.52207, longitude: -123.10894)
+    let Strain = CLLocationCoordinate2D(latitude: 45.52180, longitude: -123.10723)
+    let Aucoin = CLLocationCoordinate2D(latitude: 45.52142, longitude: -123.10982)
     var body: some View {
         Map(position: $camera) {
             UserAnnotation()
+            
+            Marker("University Center", coordinate: UCLoc)
+            Marker("Strain Science Center", coordinate: Strain)
+            Marker("Aucoin Hall", coordinate: Aucoin)
         }
         .onReceive(locationManager.$location) { location in
             guard let location, !hasCenteredOnUser else { return }
